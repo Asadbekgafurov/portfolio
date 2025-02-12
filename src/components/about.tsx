f35ad8fe1fcd8c5b2ae9ff64b0317e8a515e328a
AsadbekGofurov <114288439+Asadbekgafurov@users.noreply.github.com>
2025-02-12 20:17:08 +0500

    Update about.tsx

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -5,9 +5,9 @@ const AboutMe = () => {
     <section className="py-16 bg-white text-center">
       <h2 className="text-3xl font-semibold text-gray-900">About me</h2>
       <p className="mt-4 text-gray-700 max-w-2xl mx-auto">
-        I'm a passionate frontend developer specializing in Next.js and modern
-        web technologies. I love building user-friendly and performant web
-        applications.
+        I&#39;m a passionate frontend developer specializing in Nextjs and modern
+        web technologies I love building user-friendly and performant web
+        applications
       </p>
     </section>
   );
